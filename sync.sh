e61e072a14a2c4ffee4c95c52d9f3c89f28cdddb
Jonathan Dahan <jonathan@jedahan.com>
2014-12-30 20:07:48 -0500

    add debugging

--- sync.sh
+++ sync.sh
@@ -3,14 +3,16 @@
 directory=/Users/testuser/Desktop/Composer/
 remote=10.0.0.3
 log=transfer.log.txt
+[[ -z $DEBUG ]] && debug=echo
 
 [[ `hostname` = 'macro' ]] && dirs='LoopRec' || dirs='MicRec KeyRec Bin' 
 for remote_dir in $dirs
   i=0
   rm $log; touch $log
   for file in $directory/$remote_dir/*(om[1,20]).
-    rsync -a -- $micrec $remote:$directory/$remote_dir/
-    echo $i,"$file" >> $log
+    $debug rsync -a -- $micrec $remote:$directory/$remote_dir/
+    $debug echo $i,"$file" >> $log
+    i=(( i++ ))
   end
-  rsync -a -- $log $remote:$directory/$remote_dir/
+  $debug rsync -a -- $log $remote:$directory/$remote_dir/
 end
