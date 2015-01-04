14e995cb75a5bcdc693b324e956387a7fa479806
Jonathan Dahan <jonathan@jedahan.com>
2015-01-04 16:36:38 -0500

    add real paths and hostnames

--- sync.sh
+++ sync.sh
@@ -1,19 +1,27 @@
 #!env zsh
 
-directory=/home/micro/work/soundfx/Composer
-remote=10.0.0.3
-log=transfer.log.txt
-$DEBUG && debug=echo
+# hostnames
+composing_station=DS-SoundFXStations-1.local # (wifi: 10.0.0.49  , ethernet: 169.254.100.232)
+super_looper=DS-SoundFXStations-2.local      # (wifi: 10.0.0.202 , ethernet: 169.254.8.27)
+dj_station=DS-SoundFXStations-3.local        # (wifi: 10.0.0.34  , ethernet: 169.254.56.100)
 
-[[ `hostname` = 'macro' ]] && dirs=(MicRec KeyRec Bin) || dirs=(LoopRec)
+remote=$dj_station
+log=transfer.log.txt
+
+$DEBUG && debug=echo && prefix="$HOME" && dirs=(test)
+
+# hostnames
+[[ `hostname` = $composing_station ]] && prefix="$HOME/Desktop/Composer" && dirs=(MicRec KeyRec Bin)
+[[ `hostname` = $super_looper ]] && prefix="$HOME/Desktop/Super\ Looper" && dirs=(LoopRec)
+
 for remote_dir in $dirs; do
   i=0
-  rm $log > /dev/null
+  [[ -f $log ]] && rm $log
   touch $log
-  for file in $directory/$remote_dir/*(om[1,20]); do
-    $debug rsync -a -- $micrec $remote:$directory/$remote_dir/
-    $debug echo $i,"$file" >> $log
+  for file in $prefix/$remote_dir/*(om[1,20]); do
+    $debug rsync -a -- $file $remote:$prefix/$remote_dir/
+    echo $i,"$file" >> $log
     i=$(( i + 1 ))
   done
-  $debug rsync -a -- $log $remote:$directory/$remote_dir/
+  $debug rsync -a -- $log $remote:$prefix/$remote_dir/
 done
