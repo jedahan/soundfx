2ce40beba5048c7dacac5c9e2012967ad4493992
Jonathan Dahan <jonathan@jonathan.is>
2015-01-07 18:22:34 -0500

    be specific with rsycing

--- sync.zsh
+++ sync.zsh
@@ -5,28 +5,28 @@ composing_station=DS-SoundFXStations-1.local
 super_looper=DS-SoundFXStations-2.local
 dj_station=DS-SoundFXStations-3.local
 
-dj_prefix="$HOME/Desktop/DJ\ Station/DJBin"
 
 # dj station
 if [[ $HOST = $dj_station ]]; then
+  cd "$HOME/Desktop/DJ Station/DJBin"
   # remove any file except the 20 latest
   for file in *(om[21,-1]); do rm "$file"; done
-  log=$dj_prefix/somefile.txt
+  log=somefile.txt
   [[ -f $log ]] && rm $log
   touch $log
   i=0
-  for file in $dj_prefix/*(om[1,20]); do
+  for file in *(om[1,20]); do
+    rsync -a --rsh='ssh -p23731' $file $composing_station:$HOME/Desktop/Composer/Bin
     echo $i,"$file" >> $log
     i=$(( i + 1 ))
   done
-  rsync -a --rsh='ssh -p23731' -- "$dj_prefix" $composing_station:$HOME/Desktop/Composer/Bin
 else
   [[ $HOST = $composing_station ]] && prefix="Composer" && dirs=(MicRec KeyRec)
   [[ $HOST = $super_looper ]] && prefix="Super Looper" && dirs=(LoopRec)
 
   for dir in $dirs; do
     for file in "$HOME/Desktop/$prefix/$dir"/*(om[1,20]); do
-      rsync -a --rsh='ssh -p23733' -- "$file" $dj_station:"$dj_prefix"
+      rsync -a --rsh='ssh -p23733' -- "$file" $dj_station:"$HOME/Desktop/DJ\ Station/DJBin"
     done
   done
 fi
